aee276af50909f62ca708be20552e33ab4544ddb
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-22 12:49:29 +0530

    first commit

---
+++ src/main/java/com/infy/ci/regamqpservice/RegamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.regamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegamqpserviceApplication.class, args);
+	}
+
+}
